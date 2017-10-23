da7b7039577c14f94b9b338ff9618b8c5de84eef
ganbaatar11 <ganbaatar.gb@gmail.com>
2017-10-23 15:41:04 -0500

    customer attributes

--- src/main/java/framework/Customer.java
+++ src/main/java/framework/Customer.java
@@ -1,7 +1,22 @@
 package framework;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Customer {
+	
+	List<Account> accounts = new ArrayList<Account>();
 	private String name;
+	private String street;
+	private String city;
+	private String state;
+	private String zip;
+	private String email;
+	
+	public void addAccount(Account account) {
+		accounts.add(account);
+	}
+	
 
 	public Customer(String name) {
 		this.name = name;
@@ -14,4 +29,50 @@ public class Customer {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	@Override
+	public String toString(){
+		return getName(); 
+	}
+	
 }
