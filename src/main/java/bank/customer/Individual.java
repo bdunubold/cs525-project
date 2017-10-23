57ad9e576f52d83f14f9e70146946f30f3dc45a1
phyoko <phyothiza@gmail.com>
2017-10-23 16:22:57 -0500

    commit company indivdual

--- src/main/java/bank/customer/Individual.java
+++ src/main/java/bank/customer/Individual.java
@@ -1,12 +1,13 @@
 package bank.customer;
 
 import framework.Account;
+import framework.Address;
 import framework.Customer;
 
 import java.util.List;
 
 public class Individual extends Customer {
-	public Individual(List<Account> accounts, String name, String street, String city, String state, String zip, String email) {
-		super(accounts, name, street, city, state, zip, email);
+	public Individual(List<Account> accounts, String name, Address address) {
+		super(accounts, name, address);
 	}
 }
