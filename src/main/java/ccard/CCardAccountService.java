62a3a708114b99f7db3d9af8fb1b33c56dff6932
phyoko <phyothiza@gmail.com>
2017-10-23 17:00:37 -0500

    account service changes

--- src/main/java/ccard/CCardAccountService.java
+++ src/main/java/ccard/CCardAccountService.java
@@ -1,14 +1,52 @@
 package ccard;
 
-import framework.Account;
-import framework.AccountServiceImpl;
+import java.util.Collection;
 
-public class CCardAccountService extends AccountServiceImpl {
+import framework.Account;
+import framework.AccountDAO;
+import framework.AccountService;
+
+public class CCardAccountService extends AccountService {
+
+	public CCardAccountService(AccountDAO accountDAO) {
+		super(accountDAO);
+		// TODO Auto-generated constructor stub
+	}
 
 	@Override
 	public Account createAccount(String accountNumber, String customerName) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public Account getAccount(String accountNumber) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<Account> getAllAccounts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deposit(String accountNumber, double amount) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void withdraw(String accountNumber, double amount) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void transferFunds(String fromAccountNumber, String toAccountNumber, double amount, String description) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
--- src/main/java/framework/AccountService.java
+++ src/main/java/framework/AccountService.java
@@ -2,11 +2,32 @@ package framework;
 
 import java.util.Collection;
 
-public interface AccountService {
-    Account createAccount(String accountNumber, String customerName);
-    Account getAccount(String accountNumber);
-    Collection<Account> getAllAccounts();
-    void deposit (String accountNumber, double amount);
-    void withdraw (String accountNumber, double amount);
-    void transferFunds(String fromAccountNumber, String toAccountNumber, double amount, String description);
+public abstract class AccountService {
+
+	protected AccountDAO accountDAO;
+
+	public AccountService(AccountDAO accountDAO) {
+		this.accountDAO = accountDAO;
+
+	}
+
+	public AccountDAO getAccountDAO() {
+		return accountDAO;
+	}
+
+	public void setAccountDAO(AccountDAO accountDAO) {
+		this.accountDAO = accountDAO;
+	}
+
+	public abstract Account createAccount(String accountNumber, String customerName);
+
+	public abstract Account getAccount(String accountNumber);
+
+	public abstract Collection<Account> getAllAccounts();
+
+	public abstract void deposit(String accountNumber, double amount);
+
+	public abstract void withdraw(String accountNumber, double amount);
+
+	public abstract void transferFunds(String fromAccountNumber, String toAccountNumber, double amount, String description);
 }
