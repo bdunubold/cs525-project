ea8435d1869a5e684ac612b85150a229c4e7a8cd
AmosJindao <liusr@outlook.com>
2017-10-24 10:57:54 -0500

    m DataMap

--- src/main/java/framework/DataMap.java
+++ src/main/java/framework/DataMap.java
@@ -1,17 +1,18 @@
 package framework;
 
 public class DataMap {
-	String accounType;
-	String name;
-	String street;
-	String city;
-	String state;
-	String zip;
-	String birthday;
-	String email;
-	String numOfEmployees;
+	private AccounTypeEnum accounType;
+	private String name;
+	private String street;
+	private String city;
+	private String state;
+	private String zip;
+	private String birthday;
+	private String email;
+	private int numOfEmployees;
 	
-	public DataMap(String accounType, String name, String street, String city, String state, String zip, String email) {
+	public DataMap(AccounTypeEnum accounType, String name,
+				   String street, String city, String state, String zip, String email,int numOfEmployees) {
 		this.accounType = accounType;
 		this.name = name;
 		this.street = street;
@@ -19,62 +20,85 @@ public class DataMap {
 		this.city = city;
 		this.zip = zip;
 		this.email = email;
+        this.numOfEmployees = numOfEmployees;
 	}
-	
-	public String getAccounType() {
-		return accounType;
-	}
-	public void setAccounType(String accounType) {
-		this.accounType = accounType;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getStreet() {
-		return street;
-	}
-	public void setStreet(String street) {
-		this.street = street;
-	}
-	public String getCity() {
-		return city;
-	}
-	public void setCity(String city) {
-		this.city = city;
-	}
-	public String getState() {
-		return state;
-	}
-	public void setState(String state) {
-		this.state = state;
-	}
-	public String getZip() {
-		return zip;
-	}
-	public void setZip(String zip) {
-		this.zip = zip;
-	}
-	public String getBirthday() {
-		return birthday;
-	}
-	public void setBirthday(String birthday) {
-		this.birthday = birthday;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getNumOfEmployees() {
-		return numOfEmployees;
-	}
-	public void setNumOfEmployees(String numOfEmployees) {
-		this.numOfEmployees = numOfEmployees;
-	}
-	
-	
+
+    public AccounTypeEnum getAccounType() {
+        return accounType;
+    }
+
+    public void setAccounType(AccounTypeEnum accounType) {
+        this.accounType = accounType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getNumOfEmployees() {
+        return numOfEmployees;
+    }
+
+    public void setNumOfEmployees(int numOfEmployees) {
+        this.numOfEmployees = numOfEmployees;
+    }
+
+    @Override
+    public String toString() {
+        return "DataMap{" + "accounType=" + accounType + ", name='" + name + '\'' + ", street='" + street + '\'' + "," +
+                " city='" + city + '\'' + ", state='" + state + '\'' + ", zip='" + zip + '\'' + ", birthday='" +
+                birthday + '\'' + ", email='" + email + '\'' + ", numOfEmployees=" + numOfEmployees + '}';
+    }
 }
